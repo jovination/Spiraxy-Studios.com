df03994065be6cac4c21d5cfe772ed6be720f6a0
jovination <jovination791@gmail.com>
2025-01-06 18:44:39 +0300

    few updates

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,11 +4,11 @@ import GetStartedButton from '../components/get-started-button'
 function Hero(){
     return(
         <div>
-            <section className="w-full h-[528px] bg-[--charcoal] rounded-[32px] mt-10">
-            <div className="grid grid-cols-1 lg:grid-cols-[65%_1fr] w-full h-full p-4 md:p-8 lg:p-10">
-        <div className="flex justify-end md:justify-center flex-col space-y-6 p-4 md:p-6 lg:p-8">
-          <h1 className="text-4xl md:text-4xl lg:text-6xl font-medium text-white">
-            We Building Custom Website for Business
+            <section className="w-full h-[368px] md:h-[508px] lg:h-[528px] bg-[--charcoal] rounded-[32px] mt-10">
+            <div className="w-full flex items-end md:items-center h-full p-4 md:p-8 lg:p-10">
+        <div className="flex    flex-col space-y-6 p-4 md:p-6 lg:p-8">
+          <h1 className="text-4xl md:text-5xl lg:text-6xl font-medium text-white">
+            We Building Custom <br /> Website for Business
           </h1>
           <p className="text md:text-lg text-[--gray-faded] font-normal max-w-2xl">
             Streamline operations, drive growth, and empower your business with tailored solutions.
@@ -17,7 +17,6 @@ function Hero(){
             <GetStartedButton />
           </div>
         </div>
-        <div className="hidden  md:flex"></div>
       </div>
          </section>
         </div>   
